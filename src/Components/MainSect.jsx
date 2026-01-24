d7b4e817b20636568bd2d6dadc67cbbbb43ac035
Vishal05it <vishal.tiwari0510@gmail.com>
2026-01-24 14:59:08 +0530

    Song added

--- src/Components/MainSect.jsx
+++ src/Components/MainSect.jsx
@@ -5,7 +5,7 @@ import keyImg from "./vintage-heart-shaped-key-symbolizing-love-and-romance-png.
 import hrtImg from "./Styles/heart-removebg-preview.png";
 import rosePic from "./Styles/rose_PNG66731-removebg-preview.png";
 import sparkles from "./Styles/clearSparkles-removebg-preview.png";
-
+import song from "./Tu itni Khoobsurat Hai Reloaded - Prakriti Kakar _ Amjad Nadeem _ Jubin Nautiyal _ Gaurav Jang(M4A_128K).mp4";
 import { NavLink } from "react-router-dom";
 
 function MainSect() {
@@ -46,6 +46,7 @@ function MainSect() {
           }, 1100);
         }}
       >
+        <video src={song} autoPlay loop controls></video>
         <img src={keyImg} alt="" id="keyPic" ref={key} />
         <div
           id="imgDiv"
